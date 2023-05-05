cd8ef80bc3874c34591ba1d23bd49f1141890065
xuboM1 <xubodux@foxmail.com>
2023-05-05 09:46:50 +0800

    名片新增时限制ID取值在1-100之间

--- src/main/java/com/ruoyi/project/biz/service/impl/BizPersonalCardServiceImpl.java
+++ src/main/java/com/ruoyi/project/biz/service/impl/BizPersonalCardServiceImpl.java
@@ -1,6 +1,8 @@
 package com.ruoyi.project.biz.service.impl;
 
+import com.ruoyi.common.constant.HttpStatus;
 import com.ruoyi.common.core.text.Convert;
+import com.ruoyi.common.exception.ServiceException;
 import com.ruoyi.common.utils.DateUtils;
 import com.ruoyi.project.biz.domain.BizPersonalCard;
 import com.ruoyi.project.biz.mapper.BizPersonalCardMapper;
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 业务-个人名片Service业务层处理
@@ -51,6 +54,27 @@ public class BizPersonalCardServiceImpl implements IBizPersonalCardService {
      */
     @Override
     public int insertBizPersonalCard(BizPersonalCard bizPersonalCard) {
+        //查询出全量ID列表，判断数量是否超过100，不超过的的话，选出1-100中未被占用的
+        List<BizPersonalCard> bizPersonalCards = bizPersonalCardMapper.selectBizPersonalCardList(null);
+        Long id = 0L;
+        if (bizPersonalCards != null && bizPersonalCards.size() > 0){
+            if (bizPersonalCards.size() >= 100){
+                throw new ServiceException("名片不能添加超过100条", HttpStatus.ERROR);
+            }
+            List<Long> idList = bizPersonalCards.stream().map(BizPersonalCard::getId).collect(Collectors.toList());
+            for (long i = 1; i <= 100L; i++) {
+                if (!idList.contains(i)){
+                    id = i;
+                    break;
+                }
+            }
+        }else {
+            id = 1L;
+        }
+        if (id < 1){
+            throw new ServiceException("名片ID生成失败", HttpStatus.ERROR);
+        }
+        bizPersonalCard.setId(id);
         bizPersonalCard.setCreateTime(DateUtils.getNowDate());
         return bizPersonalCardMapper.insertBizPersonalCard(bizPersonalCard);
     }
